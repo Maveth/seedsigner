de196569ea97f0a3cb9a91814689efd034324442
Maveth <memyselfego@hotmail.com>
2023-09-10 11:14:45 -0600

    same for pub

--- src/seedsigner/helpers/nostr.py
+++ src/seedsigner/helpers/nostr.py
@@ -166,11 +166,11 @@ def sign_event_id(nostr_add: str, nostr_add_type: str, nostr_event: str):
     
     # print("LETS VERIFY THE SIGNATURE")
     # pub1="d4d4fdde8ab4924b1e452e896709a3bd236da4c0576274b52af5992d4d34762c"
-    # pub2=bytes.fromhex(pub1)
+    pub2=bytes.fromhex(pub1)
     sig2=bytes.fromhex(sig.to_string())
     # sig3=sig.to_string()
     # test=ec.secp256k1.ecdsa_verify(sig2,EVENTHASH,pub2)
-    test = ec.secp256k1.schnorrsig_verify(sig2,EVENTHASH,pub1)
+    test = ec.secp256k1.schnorrsig_verify(sig2,EVENTHASH,pub2)
     print("test is :", test)
     
     
