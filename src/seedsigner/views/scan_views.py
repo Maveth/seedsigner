ec0f14b409605f6446ebaae65ce0be8470044a5e
Maveth <memyselfego@hotmail.com>
2023-09-16 16:20:15 -0600

    bug

--- src/seedsigner/views/scan_views.py
+++ src/seedsigner/views/scan_views.py
@@ -173,6 +173,7 @@ class ScanView(View):
                 try:
                     nostr_add = self.controller.storage.get_nsec()
                     print("pulled address from storage:",nostr_add)
+                    print("same: ",self.controller.storage.get_nsec())
                 except IndexError:
                     #No Nsec is stored, goto nostr menu to add nsec
                     selected_menu_num = WarningScreen(
