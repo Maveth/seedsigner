2871cc2836ffb01c2cdd805e96cd36dc90616a8d
Maveth <memyselfego@hotmail.com>
2023-09-17 16:37:59 -0600

    bug

--- src/seedsigner/helpers/nostr.py
+++ src/seedsigner/helpers/nostr.py
@@ -180,11 +180,12 @@ def sign_event_with_key(nostr_add: str, serialized_event: str):
     """ Hashes the full_message and then signs """
     print ("trying to sign message, we have")
     print ("nostr_add: ",nostr_add)
-    print ("event",serialized_event)
+    print ("serialized event",serialized_event)
     PK1= nsec_to_hex (nostr_add[0])  #convert nsec bech32 to HEX
     PK2= ec.PrivateKey(bytes.fromhex(PK1)) #get WIF format secret privatekey used by seedsigner ec import
     
     sig = PK2.schnorr_sign(sha256(serialized_event.encode()).digest())
+    print ("we got a sig: ", sig)
     return sig
 
 
